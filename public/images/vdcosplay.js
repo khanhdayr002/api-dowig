ee6739927198828e1b07f47a9ce5278b22e14eb0
khanhdayr002 <154343426+khanhdayr002@users.noreply.github.com>
2024-08-18 10:06:55 +0700

    Update vdcosplay.js

--- public/images/vdcosplay.js
+++ public/images/vdcosplay.js
@@ -1,7 +1,7 @@
-exports.name = '/images/videochill';
+exports.name = '/images/videocosplay';
 exports.index = async(req, res, next) => {
     try {
-        const girl = require('./data/json/videochill.json');
+        const girl = require('./data/json/videocosplay.json');
         var image = girl[Math.floor(Math.random() * girl.length)].trim();
         res.jsonp({
             url: image,
