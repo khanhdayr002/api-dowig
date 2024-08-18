f5d38723040c013193d558eb73cede6af2a0154b
khanhdayr002 <154343426+khanhdayr002@users.noreply.github.com>
2024-08-18 10:28:00 +0700

    Update videochill.js

--- public/images/videochill.js
+++ public/images/videochill.js
@@ -1,7 +1,7 @@
 exports.name = '/images/videochill';
 exports.index = async(req, res, next) => {
     try {
-        const girl = require('./data/json/videochill.txt');
+        const girl = require('./data/json/videochill.json');
         var image = girl[Math.floor(Math.random() * girl.length)].trim();
         res.jsonp({
             url: image,
